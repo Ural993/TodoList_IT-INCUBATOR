ab6c5159d882361cf655d303cabb2730d4558015
Ural <ural-islamov@mail.ru>
2021-11-29 21:50:53 +0500

    8 lesson, add useReducer

---
+++ src/state/todolistsReducer.ts
@@ -0,0 +1,44 @@
+import {FilterValuesType, TodolistType} from "../App";
+import {v1} from "uuid";
+
+type RemoveTodolistActionType = {
+    type: 'REMOVE-TODOLIST'
+    id: string
+}
+type AddTodolistActionType = {
+    type: 'ADD-TODOLIST'
+    title: string
+}
+type ChangeTodolistTitleActionType = {
+    type: 'CHANGE-TODOLIST-TITLE'
+    title: string
+    id: string
+}
+type ChangeTodolistFilterActionType = {
+    type: 'CHANGE-TODOLIST-FILTER'
+    filter: FilterValuesType
+    id: string
+}
+type ActionType =
+    RemoveTodolistActionType
+    | AddTodolistActionType
+    | ChangeTodolistTitleActionType
+    | ChangeTodolistFilterActionType
+export const todolistsReducer = (state: Array<TodolistType>, action: ActionType): Array<TodolistType> => {
+    switch (action.type) {
+        case 'REMOVE-TODOLIST':
+            return state.filter(t => t.id !== action.id)
+        case 'ADD-TODOLIST':
+            return [...state, {id: v1(), title: action.title, filter: 'all'}]
+        case 'CHANGE-TODOLIST-TITLE':
+            return state.map(t => (t.id === action.id ? {...t, title: action.title} : t))
+        case 'CHANGE-TODOLIST-FILTER':
+            return state.map(t => (t.id === action.id ? {...t, filter: action.filter} : t))
+        default :
+            return state
+    }
+
+}
+export const RemoveTodolistAC = (todolistId: string): RemoveTodolistActionType => {
+    return {type: 'REMOVE-TODOLIST', id: todolistId}
+}
